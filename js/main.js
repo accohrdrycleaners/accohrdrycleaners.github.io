7dd837afb649138873c8c9424d58f95f873836f7
dishant.wadhwani <dishant.wadhwani@dishantwadhwanis-MacBook-Pro.local>
2023-10-23 11:51:27 +0530

    remaing pages changes

--- js/main.js
+++ js/main.js
@@ -140,3 +140,13 @@
     
 })(jQuery);
 
+
+// FAQ
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
+
